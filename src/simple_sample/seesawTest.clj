04c9f1953f75f7ca4c39e0b74c4c97bc522a1d26
Giovanni Ferrari <ferrarigiovanni555@gmail.com>
2017-04-22 20:32:29 +0200

    studing the difference between RGB and HSV

--- src/simple_sample/seesawTest.clj
+++ src/simple_sample/seesawTest.clj
@@ -3,8 +3,9 @@
                    [org.opencv.imgcodecs Imgcodecs]
                    [org.opencv.imgproc Imgproc]
                    [java.awt.image BufferedImage]
-		   [javax.swing ImageIcon]
-		   ))
+		               [javax.swing ImageIcon]
+                   )
+          (:require [simple-sample.utils :as ssu]))
 
 (use 'clojure.repl)
 (use 'seesaw.core)
@@ -17,32 +18,10 @@
 (defn ReadImg [image_name]
   (Imgcodecs/imread (str images_path image_name)))
 
-(def mat (ReadImg "BASIL.JPG"))
-
-(println (.width mat))
+(def mat (ReadImg "tree.jpg"))
 
 (def bufferImageFromMat (atom (BufferedImage. (.width mat) (.height mat) 1)))
 
-
-(defn to-buffered-image [mat]
-  (let [out (Mat.)
-        colour? (< 1 (.channels mat))
-        type (if colour?
-               java.awt.image.BufferedImage/TYPE_3BYTE_BGR
-               java.awt.image.BufferedImage/TYPE_BYTE_GRAY)
-        width (.cols mat)
-        height (.rows mat)]
-    (do
-      (if colour?
-        (Imgproc/cvtColor mat out Imgproc/COLOR_BGR2RGB)
-        (.copyTo mat out))
-      (let [blen (* (.channels mat) width height)
-            bytes (byte-array blen)]
-        (.get out 0 0 bytes)
-        (let [image (java.awt.image.BufferedImage. width height type)
-              raster (.getRaster image)]
-          (.setDataElements raster 0 0 width height bytes)
-image)))))
 
 (def f (frame :title "Test SeeSaw Robonica"
               :size [300 :by 300]
@@ -54,7 +33,7 @@ image)))))
 (def sldr (slider :orientation :vertical))
 
 ;(def lbl (label :icon (ImageIcon. "resources/images/Basil_gray.png")))
-(def lbl (label :icon (ImageIcon. (to-buffered-image mat))))
+(def lbl (label :icon (ImageIcon. (ssu/to-buffered-image mat))))
 
 (def split (left-right-split sldr lbl :divider-location 1/3))
 
@@ -66,10 +45,9 @@ image)))))
 
 (def mat2 (Mat.))
 (listen sldr :mouse-released (fn [e] (do (Imgproc/blur mat mat2 (Size. (/ (value e) 5) (/ (value e) 5) ))
-					(config! lbl :icon (ImageIcon. (to-buffered-image mat2))))))
+					(config! lbl :icon (ImageIcon. (ssu/to-buffered-image mat2))))))
 
 (display split)
 ;(display lbl)
 
-(-> f pack! show! )
-
+;(-> f pack! show! )
