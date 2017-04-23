06ec6705ba8ea101c6e4f7dc1a315c53cf4cc621
Giovanni Ferrari <ferrarigiovanni555@gmail.com>
2017-04-23 18:09:36 +0200

    added draft of remove-background function

--- src/simple_sample/utils.clj
+++ src/simple_sample/utils.clj
@@ -70,7 +70,7 @@
      new_mat)))
 
 ;
-(defn toHVS
+(defn toHSV
   "take a matrix object and return a new one in Hue Saturation Value color model."
   [mat]
   (let [new-mat (Mat.)
@@ -78,12 +78,19 @@
     new-mat))
 ;
 
+(defn toLab
+  "take a matrix object and return a new one in Hue Saturation Value color model."
+  [mat]
+  (let [new-mat (Mat.)
+        _ (Imgproc/cvtColor mat new-mat Imgproc/COLOR_BGR2Lab)]
+    new-mat))
+
 ;(Core/inRange mat (Scalar. 0 255 100) (Scalar. 255 255 255) new-mat)
 (defn test-HSV
   "only a temporary function to make some test about HSV property of an image
   IT HAS A BIG PROBLEM WHEN LOWER LIMIT IS SETTLED TO 0"
   [mat]
-  (let [old-mat (toHVS mat)
+  (let [old-mat (toHSV mat)
         f (frame :title "mat" :size [100 :by 100])
         sldr-ll (slider :orientation :vertical :max 255 :value 0) ;;slider of lower limit
         sldr-ul (slider :orientation :vertical :max 255 :value 255) ;;slider of upper limit
@@ -111,3 +118,50 @@
                               (println "value of upper limit: " (value e)))))
         _ (config! f :content (horizontal-panel :items [sldr-ll sldr-ul lbl]))]
     (-> f pack! show!)))
+;
+(defn split-mat
+  "take a mat with three channel and return a java.util.ArrayList object
+  containing three mat, one for each channel of the original matrix"
+  [mat]
+  (let [mat-array (java.util.ArrayList.)
+        _ (Core/split mat mat-array)]
+    mat-array))
+
+(defn in-range
+  [mat low-limit up-limit]
+  (let [new-mat (Mat.)
+        _ (Core/inRange mat (Scalar. low-limit 0 0) (Scalar. up-limit 0 0) new-mat)]
+    new-mat))
+
+;
+(defn gaussian-blur
+  [mat square-side sigma]
+  (let [new-mat (Mat.)
+        _ (Imgproc/GaussianBlur mat new-mat (Size. square-side square-side) sigma)]
+    new-mat))
+
+(defn median-blur
+  [mat sigma]
+  (let [new-mat (Mat.)
+        _ (Imgproc/medianBlur mat new-mat sigma)]
+    new-mat))
+
+(defn bitwise-or
+  [mat1 mat2 & mat-list]
+  (let [new-mat (Mat.)
+        _ (Core/bitwise_or mat1 mat2 new-mat)]
+    (if-not (empty? mat-list) (apply bitwise-or new-mat (first mat-list) (rest mat-list)) new-mat)))
+
+
+(defn remove-background
+  [mat]
+  (let [new-mat (Mat.)
+        threeshold-sHSV-channel (in-range (second (split-mat (toHSV mat))) 130 255)
+        threeshold-bLab-channel (in-range (second (rest (split-mat (toLab mat)))) 135 255)
+        mask-img (bitwise-or threeshold-bLab-channel threeshold-sHSV-channel)
+        _ (.copyTo mat new-mat mask-img)]
+        new-mat))
+
+
+;WRITE A MACRO a new def-withNewMat that set the function directly with the new variable new-mat,
+;necessary to preserve immutability
